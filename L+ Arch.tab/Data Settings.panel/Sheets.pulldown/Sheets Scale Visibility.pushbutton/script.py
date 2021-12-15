55421d1465aeb2609726e56d933ca32165ad6ac7
sanglc94 <86165045+sanglc94@users.noreply.github.com>
2021-12-15 15:33:27 +0700

    Update Sheet Scale Setting

--- L+ Arch.tab/Data Settings.panel/Sheets.pulldown/Sheets Scale Visibility.pushbutton/script.py
+++ L+ Arch.tab/Data Settings.panel/Sheets.pulldown/Sheets Scale Visibility.pushbutton/script.py
@@ -1,6 +1,6 @@
 __doc__ = 'Python for revit api'
 __author__ = 'L+ Arch'
-from Autodesk.Revit.DB import Transaction, FilteredElementCollector,BuiltInCategory
+from Autodesk.Revit.DB import Transaction, FilteredElementCollector,BuiltInCategory,TransactionGroup
 from Autodesk.Revit.UI import UIDocument, Selection
 
 uidoc = __revit__.ActiveUIDocument
@@ -8,6 +8,28 @@ doc = __revit__.ActiveUIDocument.Document
 
 titleblock_col = FilteredElementCollector(doc).OfCategory(BuiltInCategory.OST_TitleBlocks).WhereElementIsNotElementType().ToElements()
 
+tg = TransactionGroup(doc, "Sheets Scale Setting")
+tg.Start()
+t1 = Transaction(doc, "Scale Text Setting")
+t1.Start()
+for tb in titleblock_col:
+    param = tb.LookupParameter("Scale")
+    paramVisibility = tb.LookupParameter("Scale Visibility")
+    viewSheetId = tb.OwnerViewId
+    viewSheet = doc.GetElement(viewSheetId)
+    paramText = viewSheet.LookupParameter("Scale Text")
+    allViewsport = viewSheet.GetAllViewports()
+    if allViewsport:
+        firstViewportId = allViewsport[0]
+        firstViewport = doc.GetElement(firstViewportId)
+        viewId = firstViewport.ViewId
+        firstView = doc.GetElement(viewId)
+        scale = firstView.Scale
+        if paramText != None:
+            paramText.Set("1 : " + str(scale))
+
+t1.Commit()
+        
 t = Transaction(doc, "Scale Visibility")
 t.Start()
 
@@ -19,5 +41,7 @@ for tb in titleblock_col:
             paramVisibility.Set(1)
         else:
             paramVisibility.Set(0)
-
+            
+    
 t.Commit()
+tg.Assimilate()
